9f17fe584f820a1790b2e8550dad7c48c60af6dc
SKY <sarakyeo@gmail.com>
2025-09-22 15:38:56 -0600

    working on infoseek DV

--- scripts/02_DVs.R
+++ scripts/02_DVs.R
@@ -110,4 +110,119 @@ descr(clean$curious) # M = 4.79, SD = 1.63
 
 
 # Information Seeking -----------------------
+# Simple/Fabrics
+clean |> 
+    select(Q30_1:Q30_5) |> 
+    freq()
+clean <- var_recode(data = clean, vars = c(Q30_1:Q30_5))
+clean |> 
+    select(Q30_1c:Q30_5c) |> 
+    freq()
 
+# Complex/Fabrics
+clean |> 
+    select(Q45_1:Q45_5) |> 
+    freq()
+clean <- var_recode(data = clean, vars = c(Q45_1:Q45_5))
+clean |> 
+    select(Q45_1c:Q45_5c) |> 
+    freq()
+
+# Simple/Makeup
+clean |> 
+    select(Q60_1:Q60_5) |> 
+    freq()
+clean <- var_recode(data = clean, vars = c(Q60_1:Q60_5))
+clean |> 
+    select(Q60_1c:Q60_5c) |> 
+    freq()
+
+# Complex/Makeup
+clean |> 
+    select(Q75_1:Q75_5) |> 
+    freq()
+clean <- var_recode(data = clean, vars = c(Q75_1:Q75_5))
+clean |> 
+    select(Q75_1c:Q75_5c) |> 
+    freq()
+
+# Combine into a single columns -------------
+clean <- clean |>
+    mutate(infoseek1 = case_when(
+        Q30_1c == Q30_1c ~ Q30_1c,
+        Q45_1c == Q45_1c ~ Q45_1c,
+        Q60_1c == Q60_1c ~ Q60_1c,
+        Q75_1c == Q75_1c ~ Q75_1c
+    ))
+clean |> freq(infoseek1)
+
+clean <- clean |>
+    mutate(infoseek2 = case_when(
+        Q30_2c == Q30_2c ~ Q30_2c,
+        Q45_2c == Q45_2c ~ Q45_2c,
+        Q60_2c == Q60_2c ~ Q60_2c,
+        Q75_2c == Q75_2c ~ Q75_2c
+    ))
+clean |> freq(infoseek2)
+
+clean <- clean |>
+    mutate(infoseek3 = case_when(
+        Q30_3c == Q30_3c ~ Q30_3c,
+        Q45_3c == Q45_3c ~ Q45_3c,
+        Q60_3c == Q60_3c ~ Q60_3c,
+        Q75_3c == Q75_3c ~ Q75_3c
+    ))
+clean |> freq(infoseek3)
+
+clean <- clean |>
+    mutate(infoseek4 = case_when(
+        Q30_4c == Q30_4c ~ Q30_4c,
+        Q45_4c == Q45_4c ~ Q45_4c,
+        Q60_4c == Q60_4c ~ Q60_4c,
+        Q75_4c == Q75_4c ~ Q75_4c
+    ))
+clean |> freq(infoseek4)
+
+clean <- clean |>
+    mutate(infoseek5 = case_when(
+        Q30_5c == Q30_5c ~ Q30_5c,
+        Q45_5c == Q45_5c ~ Q45_5c,
+        Q60_5c == Q60_5c ~ Q60_5c,
+        Q75_5c == Q75_5c ~ Q75_5c
+    ))
+clean |> freq(infoseek5)
+
+## Factor analysis on infoseek items -------------
+clean |>
+    select(infoseek1:infoseek5) |>
+    cortest.bartlett() # sig.
+
+clean |>
+    select(infoseek1:infoseek5) |>
+    KMO() # Overall MSA = .86
+
+clean |>
+    select(infoseek1:infoseek5) |>
+    fa.parallel() # 2 factors, 1 component
+
+clean |>
+    select(infoseek1:infoseek5) |>
+    fa(.,
+        nfactors = 1,
+        rotate = "promax",
+        fm = "pa"
+    ) |>
+    fa.diagram() # single factor solution has better factor loadings
+
+## Cronbach's alpha -------------
+clean |>
+    select(infoseek1:infoseek5) |>
+    alpha() # Cronbach's alpha = 0.91
+
+clean <- clean |>
+    rowwise() |>
+    mutate(
+        infoseek = mean(c(infoseek1, infoseek2, infoseek3, infoseek4, infoseek5), na.rm = TRUE)
+    )
+clean |> freq(infoseek)
+descr(clean$infoseek) # M = 4.24, SD = 1.70
