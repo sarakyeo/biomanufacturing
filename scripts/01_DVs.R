f110bb1d207cb20e1a80f8be0ba667c6613c8723
SKY <sarakyeo@gmail.com>
2025-09-22 14:30:00 -0600

    working on creating DVs

--- scripts/01_DVs.R
+++ scripts/01_DVs.R
@@ -17,3 +17,21 @@ clean <- var_recode(data = clean, vars = Q44_1:Q44_4)
 clean |>
     select(Q44_1c:Q44_4c) |>
     freq()
+
+# Simple/Makeup
+clean |>
+    select(Q59_1:Q59_4) |>
+    freq()
+clean <- var_recode(data = clean, vars = Q59_1:Q59_4)
+clean |>
+    select(Q59_1c:Q59_4c) |>
+    freq()
+
+# Complex/Makeup
+clean |>
+    select(Q74_1:Q74_4) |>
+    freq() 
+clean <- var_recode(data = clean, vars = Q74_1:Q74_4)
+clean |>
+    select(Q74_1c:Q74_4c) |>
+    freq()
