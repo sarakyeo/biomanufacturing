d159417d18b2e3a8d5151ac8890ede8d3fe2c8d2
Sara K Yeo <sarakyeo@gmail.com>
2025-11-18 16:09:34 -0700

    worked on table and figures for J Risk Research potential paper

---
+++ scripts/04_OLS-models.R
@@ -0,0 +1,82 @@
+
+m0 <- clean |> 
+  select(support, female, defn, issue, risks, benefits) |> 
+  na.omit() |> 
+  lm(formula = support ~ defn + issue + female + risks + benefits)
+m0 |> summ(center = TRUE)
+
+m1 <- clean |> 
+  select(support, female, defn, issue, risks, benefits) |> 
+  na.omit() |> 
+  lm(formula = support ~ defn + issue + female + risks + benefits +
+    defn:issue + female:defn + female:issue)
+m1 |> summ(center = TRUE)
+
+m2 <- clean |> 
+  select(support, female, defn, issue, risks, benefits) |> 
+  na.omit() |> 
+  lm(formula = support ~ defn + issue + female + risks + benefits +
+    defn:issue + female:defn + female:issue +
+    defn:issue:female)
+m2 |> summ(center = TRUE)
+
+anova(m0, m1, m2)
+
+export_summs(m0, m1, m2,
+             model.names = c("Model 1", "Model 2", "Model 3"),
+             number_format = 2,
+             ci_level = .95,
+             align = ".",
+             statistics = c("N" = "nobs",
+                            "Adj. R-squared"= "adj.r.squared",
+                            "F" = "statistic",
+                            "df" = "df",
+                            "p" = "p.value"
+                          ),
+             error_format = "({std.error})",
+             error_pos = c("same"),
+             coefs = c("(Intercept)" = "(Intercept)",
+                       "Definition manipulation (complex)" = "defncomplex",
+                       "Issue manipulation (makeup)" = "issuemakeup",
+                       "Gender (female)" = "femalefemale",
+                       "Perceived risks" = "risks",
+                       "Perceived benefits" = "benefits",
+                       "Definition × Gender" = "defncomplex:femalefemale",
+                       "Issue × Gender" = "issuemakeup:femalefemale",
+                       "Definition × Issue" = "defncomplex:issuemakeup",
+                       "Definition × Issue × Gender" = "defncomplex:issuemakeup:femalefemale"
+                      ))|> 
+        set_all_padding(0) |> 
+        set_label("tab:OLS-model") |> 
+        set_caption("Unstandardized regression coefficients and standard errors (in parentheses) in the OLS model predicting support for biomanufacturing research.") |> 
+        set_font_size(11) |> 
+        print_latex() |> 
+        capture.output(file = here::here("outputs", "tab-OLS-model.tex"))
+
+ixn.plot <- cat_plot(
+    model = mols,
+    pred = defn,
+    modx = female,
+    mod2 = issue,
+    interval = TRUE,
+    int.type = "confidence",
+    int.width = .95,
+    x.label = "Definition Type",
+    y.label = "Support for biomanufacturing research",
+    mod2.labels = c("fabric", "makeup"),
+    line.thickness = 0.5,
+    pred.point.size = 1.5,
+    errorbar.width = 0.25,
+    colors = c("grey", "black")
+  ) +
+  theme_apa() + 
+  scale_y_continuous(
+    limits = c(1,7),
+    expand = c(0,0),
+    breaks = seq(1,7, 1)
+  )
+
+ggsave(plot = ixn.plot,
+       here::here("outputs", "int-fig.png"),
+       width = 10,
+       height = 5)
--- scripts/04_PROCESS-models.R
+++ scripts/04_PROCESS-models.R
@@ -61,8 +61,8 @@ m1 <- process(
   y = "support",
   x = "ndefn",
   m = c("risks", "benefits"),
-  cov = c("nissue", "nfemale"),
-  model = 4,
+  w = "nissue",
+  model = 8,
   total = 1,
   contrast = 1,
   progress = 0,
@@ -85,9 +85,9 @@ m2 <- process(
   y = "support",
   x = "ndefn",
   m = c("risks", "benefits"),
-  w = "nfemale",
-  cov = "nissue",
-  model = 8,
+  w = "nissue",
+  z = "nfemale",
+  model = 12,
   total = 1,
   contrast = 1,
   progress = 0,
@@ -95,70 +95,3 @@ m2 <- process(
   boot = 10000,
   seed = 20251111
 )
-
-
-mols <- clean |> 
-  select(support, female, defn, issue, risks, benefits) |> 
-  na.omit() |> 
-  lm(formula = support ~ defn + issue + female + risks + benefits + 
-    female:defn + female:issue + defn:issue + defn:issue:female)
-
-mols |> summ(center = TRUE)
-
-export_summs(mols,
-             model.names = "Support for biomanufacturing research",
-             number_format = 2,
-             ci_level = .95,
-             align = ".",
-             statistics = c("N" = "nobs",
-                            "Adj. R-squared"= "adj.r.squared",
-                            "F" = "statistic",
-                            "df" = "df"),
-             error_format = "({std.error})",
-             error_pos = c("same"),
-             coefs = c("(Intercept)" = "(Intercept)",
-                       "Definition manipulation (complex)" = "defncomplex",
-                       "Issue manipulation (makeup)" = "issuemakeup",
-                       "Gender (female)" = "femalefemale",
-                       "Perceived risks" = "risks",
-                       "Perceived benefits" = "benefits",
-                       "Definition × Gender" = "defncomplex:femalefemale",
-                       "Issue × Gender" = "issuemakeup:femalefemale",
-                       "Definition × Issue" = "defncomplex:issuemakeup",
-                       "Definition × Issue × Gender" = "defncomplex:issuemakeup:femalefemale"
-                      ))|> 
-        set_all_padding(0) |> 
-        set_label("tab:OLS-model") |> 
-        set_caption("Unstandardized regression coefficients and standard errors (in parentheses) in the OLS model predicting support for biomanufacturing research.") |> 
-        set_font_size(11) |> 
-        print_latex() |> 
-        capture.output(file = here::here("outputs", "tab-motiv-total-eff.tex"))
-
-ixn.plot <- cat_plot(
-    model = mols,
-    pred = defn,
-    modx = female,
-    mod2 = issue,
-    interval = TRUE,
-    int.type = "confidence",
-    int.width = .95,
-    x.label = "Definition complexity",
-    y.label = "Support for biomanufacturing research",
-    mod2.labels = c("Fabric", "Makeup"),
-    line.thickness = 0.5,
-    pred.point.size = 1.5,
-    errorbar.width = 0.25,
-    colors = c("grey", "black")
-  ) +
-  theme_apa() + 
-  scale_y_continuous(
-    limits = c(1,7),
-    expand = c(0,0),
-    breaks = seq(1,7, 1)
-  )
-
-ggsave(plot = ixn.plot,
-       here::here("outputs", "int-fig.png"),
-       width = 10,
-       height = 5)
-
