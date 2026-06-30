9b4804e7d9396cc99bb6a70b595649bff7a22852
Sara K Yeo <sarakyeo@gmail.com>
2026-06-30 12:32:01 -0600

    working on Risk Analysis R&R

--- scripts/03_ANOVA.R
+++ scripts/03_ANOVA.R
@@ -108,7 +108,9 @@ clean |>
   group_by(defn) |> 
   descr(risks) # simple: M = 4.35, SD = 1.64; complex: M = 4.63, SD = 1.54
 
-mean.risks <- clean |>
+# Graphing mean differences by condition -----------------
+## Definition ------------
+mean.risks.defn <- clean |>
   select(defn, risks) |>
   na.omit() |>
   ggplot(aes(x = defn)) +
@@ -128,13 +130,13 @@ mean.risks <- clean |>
   ) +
   jtools::theme_apa() +
   theme(
-    axis.title.y = element_text(size = 18),
-    axis.text.y = element_text(size = 18, color = "black"),
-    axis.title.x = element_text(size = 18),
-    axis.text.x = element_text(size = 18, color = "black")
+    axis.title.y = element_text(size = 13),
+    axis.text.y = element_text(size = 13, color = "black"),
+    axis.title.x = element_text(size = 13),
+    axis.text.x = element_text(size = 13, color = "black")
   ) 
 
-mean.bens <- clean |>
+mean.bens.defn <- clean |>
   select(defn, benefits) |>
   na.omit() |>
   ggplot(aes(x = defn)) +
@@ -154,26 +156,129 @@ mean.bens <- clean |>
   ) +
   jtools::theme_apa() +
   theme(
-    axis.title.y = element_text(size = 18),
-    axis.text.y = element_text(size = 18, color = "black"),
-    axis.title.x = element_text(size = 18),
-    axis.text.x = element_text(size = 18, color = "black")
+    axis.title.y = element_text(size = 13),
+    axis.text.y = element_text(size = 13, color = "black"),
+    axis.title.x = element_text(size = 13),
+    axis.text.x = element_text(size = 13, color = "black")
   ) 
 
-ggsave(mean.bens,
-       filename = "means-bens-plot.png",
+mean.supp.defn <- clean |>
+  select(defn, support) |>
+  na.omit() |>
+  ggplot(aes(x = defn)) +
+  stat_summary(aes(y = support), fun = mean, geom = "point") +
+  stat_summary(
+    aes(y = support),
+    fun.data = mean_cl_normal,
+    geom = "errorbar",
+    width = 0.2
+  ) +
+  scale_x_discrete(name = "Definition", labels = c("simple", "complex")) +
+  scale_y_continuous(
+    name = "Support for biomanufacturing research",
+    expand = c(0, 0),
+    limits = c(1, 7),
+    breaks = seq(1, 7, 1)
+  ) +
+  jtools::theme_apa() +
+  theme(
+    axis.title.y = element_text(size = 13),
+    axis.text.y = element_text(size = 13, color = "black"),
+    axis.title.x = element_text(size = 13),
+    axis.text.x = element_text(size = 13, color = "black")
+  )
+
+## Issue -----------------------
+mean.risks.issue <- clean |>
+  select(issue, risks) |>
+  na.omit() |>
+  ggplot(aes(x = issue)) +
+  stat_summary(aes(y = risks), fun = mean, geom = "point") +
+  stat_summary(
+    aes(y = risks),
+    fun.data = mean_cl_normal,
+    geom = "errorbar",
+    width = 0.2
+  ) +
+  scale_x_discrete(name = "Issue", labels = c("fabric", "makeup")) +
+  scale_y_continuous(
+    name = "Perceived risks",
+    expand = c(0, 0),
+    limits = c(1, 7),
+    breaks = seq(1, 7, 1)
+  ) +
+  jtools::theme_apa() +
+  theme(
+    axis.title.y = element_text(size = 13),
+    axis.text.y = element_text(size = 13, color = "black"),
+    axis.title.x = element_text(size = 13),
+    axis.text.x = element_text(size = 13, color = "black")
+  ) 
+
+mean.bens.issue <- clean |>
+  select(issue, benefits) |>
+  na.omit() |>
+  ggplot(aes(x = issue)) +
+  stat_summary(aes(y = benefits), fun = mean, geom = "point") +
+  stat_summary(
+    aes(y = benefits),
+    fun.data = mean_cl_normal,
+    geom = "errorbar",
+    width = 0.2
+  ) +
+  scale_x_discrete(name = "Issue", labels = c("fabric", "makeup")) +
+  scale_y_continuous(
+    name = "Perceived benefits",
+    expand = c(0, 0),
+    limits = c(1, 7),
+    breaks = seq(1, 7, 1)
+  ) +
+  jtools::theme_apa() +
+  theme(
+    axis.title.y = element_text(size = 13),
+    axis.text.y = element_text(size = 13, color = "black"),
+    axis.title.x = element_text(size = 13),
+    axis.text.x = element_text(size = 13, color = "black")
+  ) 
+
+mean.supp.issue <- clean |>
+  select(issue, support) |>
+  na.omit() |>
+  ggplot(aes(x = issue)) +
+  stat_summary(aes(y = support), fun = mean, geom = "point") +
+  stat_summary(
+    aes(y = support),
+    fun.data = mean_cl_normal,
+    geom = "errorbar",
+    width = 0.2
+  ) +
+  scale_x_discrete(name = "Issue", labels = c("fabric", "makeup")) +
+  scale_y_continuous(
+    name = "Support for biomanufacturing research",
+    expand = c(0, 0),
+    limits = c(1, 7),
+    breaks = seq(1, 7, 1)
+  ) +
+  jtools::theme_apa() +
+  theme(
+    axis.title.y = element_text(size = 13),
+    axis.text.y = element_text(size = 13, color = "black"),
+    axis.title.x = element_text(size = 13),
+    axis.text.x = element_text(size = 13, color = "black")
+  )
+
+mean.plots <- ggpubr::ggarrange(
+  mean.risks.defn, mean.risks.issue,
+  mean.bens.defn, mean.bens.issue,
+  mean.supp.defn, mean.supp.issue,
+  nrow = 3, ncol = 2,
+  align = "hv"
+)
+
+ggsave(mean.plots,
+       filename = "means-plots.png",
        path = "outputs",
        device = "png",
-       width = 10,
+       width = 7,
        height = 10,
        units = "in")
-
-# clean |>
-#   select(issue, risks) |>
-#   na.omit() |>
-#   anova_test(risks ~ issue, effect.size = "pes", detailed = TRUE) # ns
-
-# clean |>
-#   select(defn, issue, risks) |>
-#   na.omit() |>
-#   anova_test(risks ~ defn + issue + defn:issue, effect.size = "pes", detailed = TRUE) # ns
